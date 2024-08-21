f65b7c4f2b8907fe39b614e78d3d2950cbc0d297
MatteoDiMario96 <dm.matteo96@live.it>
2024-08-21 16:35:22 +0200

    Creato metodo index che mi prende tutti i progetti e mi pagina 3 x 3 e mi ritorna un JSON. Creato un metodo show importato il suo modello, relazionato grazie a loadMissing anche type e technologies e ritornato un JSON. Creato un metoso userSearch, creata una variabile che prende tutte le chiamate; poi fatto una serie di if e elseif che se presente data...project_name, project_created_at, technology_name, type_name di cercare quello che l' utente ha cercato. Nella tabella, colonna giusta.

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        //RITORNA UN JSON CON X COSE
+        $projects = Project::paginate(3);
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $projects
+            ]);
+    }
+
+    public function show(Project $project){
+        //RITORNA UN JSON CON Y COSE
+        $project->loadMissing("type", "technologies");
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $project
+            ]);
+    }
+
+    public function userSearch(Request $request){
+        //RITORNA UN JSON CON Y COSE
+        $data = $request->all();
+        if (isset($data['project_name'])) {
+            $searchedNameProject = Project::with("type", "technologies")->where("name", "LIKE", "%" . ($data["project_name"] ?? "") . "%")->get();
+            return response()->json(
+                [
+                    "success" => true,
+                    "results" => $searchedNameProject
+                ]);
+        } elseif (isset($data['project_created_at'])) {
+            $searchedDateProject = Project::with("type", "technologies")->where("project_created_at", "LIKE", "%" . ($data["project_created_at"] ?? "") . "%")->get();
+            return response()->json(
+                [
+                    "success" => true,
+                    "results" => $searchedDateProject
+                ]);
+        } elseif (isset($data['technology_name'])) {
+            $searchTechnology = Technology::with("projects")->where("name", "LIKE", "%" . ($data["technology_name"] ?? "") . "%")->get();
+            return response()->json(
+                [
+                    "success" => true,
+                    "results" => $searchTechnology
+                ]);
+        } elseif (isset($data['type_name'])) {
+            $searchType = Type::with("projects")->where("name", "LIKE", "%" . ($data["type_name"] ?? "") . "%")->get();
+            return response()->json(
+                [
+                    "success" => true,
+                    "results" => $searchType
+                ]);
+        }
+    }
+}
